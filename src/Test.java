aaaa4d2c8a6dbe5c28d974c81b0e36b4cc964e6c
saidshah <saidmshah93@gmail.com>
2023-02-09 09:11:39 -0600

    first exercise

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class  Test {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        String  Hello = "HEllo";
+        System.out.println(Hello);
+        int x = 5;
+        int y = 4;
+        int z = x + y;
+        System.out.println(z);
+        int arr[] = {1, 2, 3, 4};
+        for(int ar : arr){
+            System.out.println(ar);
+        }
+
+    }
+}
