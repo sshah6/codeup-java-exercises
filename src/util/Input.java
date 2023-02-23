9d640183ae485fe52e58ca900e01515259bd895c
saidshah <saidmshah93@gmail.com>
2023-02-23 13:50:23 -0600

    Exception ex

--- src/util/Input.java
+++ src/util/Input.java
@@ -14,10 +14,12 @@ public class Input{
     private double doubleMinMax;
 
     public String getString() {
-        System.out.println("Enter String y/n.");
+//        System.out.println("Enter String y/n.");
+        System.out.println("Enter a number or integer 1 - 10 to see the error: ");
         stringInput = scanner.nextLine();
         return this.stringInput;
     }
+
 
     public boolean yesNo() {
         String stringInput = getString();
@@ -30,13 +32,19 @@ public class Input{
     }
 
     public int getInt(int min, int max){
-        System.out.println("Eneter a number between " + min +  " and "  + max + "\n");
-        userMinMax = scanner.nextInt();
-        if(userMinMax > max || userMinMax  < min){
-            return getInt(min, max);
-        } else{
-            return this.userMinMax;
+//        System.out.println("Eneter a number between " + min +  " and "  + max + "\n");
+//        userMinMax = scanner.nextInt(); //instead of this i put the below code
+        try {
+            userMinMax = Integer.parseInt(stringInput);
+            if(userMinMax > max || userMinMax  < min){
+                return getInt(min, max);
+            } else{
+                return this.userMinMax;
+            }
+        }catch(NumberFormatException nfe){
+            System.out.println("Error: You did not enter a integer type: " + nfe);
         }
+        return this.userMinMax;
     }
 
     int getInt(){
@@ -44,14 +52,21 @@ public class Input{
     }
 
     public double getDouble(double min, double max){
-        System.out.println("Enter a decimal number between " + min + " max " + max + "\n");
-        doubleMinMax = scanner.nextDouble();
-        if(doubleMinMax > max || doubleMinMax < min){
-            return getDouble(min, max);
-        }else{
-            return doubleMinMax;
+//        System.out.println("Enter a decimal number between " + min + " max " + max + "\n");
+//        doubleMinMax = scanner.nextDouble();  //instead of double we use the getstring input
+        try {
+            doubleMinMax = Double.parseDouble(stringInput);
+            if(doubleMinMax > max || doubleMinMax < min){
+                return getDouble(min, max);
+            }else{
+                return doubleMinMax;
+            }
+        }catch(NumberFormatException nfe){
+            System.out.println("Error: You did not enter a double type: " + nfe);
         }
+        return this.doubleMinMax;
     }
+
 
     public double getDouble(){
         return doubleMinMax;
